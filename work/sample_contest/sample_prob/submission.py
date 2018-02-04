4cbe4151ffee933952016d695a50ca9b49c3a7b7
kjnh10 <kojinho10@gmail.com>
2018-02-04 12:19:19 +0900

    test.pyを同一フォルダにおいて使用する場合

---
+++ work/sample_contest/sample_prob/submission.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+IN = 'sample-1.in'  # this script's input
+try:  # {{{
+    from test import set_stdin
+    import sys
+    if len(sys.argv) != 1:  # when stdin is not refered to file
+        IN = sys.argv[1]
+    set_stdin(IN)
+except:
+    pass  # }}}
+
+# standard library# {{{
+import sys
+import os
+import time
+import re
+from pydoc import help
+import string
+import math
+import numpy as np
+from operator import itemgetter
+from collections import Counter
+from collections import deque
+from collections import defaultdict as dd
+import fractions
+from heapq import heappop, heappush, heapify
+import array
+from bisect import bisect_left, bisect_right, insort_left, insort_right
+from copy import deepcopy as dcopy
+import itertools
+sys.setrecursionlimit(10**7)
+inf = 10**20
+gosa = 1.0 / 10**10
+mod = 10**9+7
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LI_(): return [int(x)-1 for x in sys.stdin.readline().split()]
+def LF(): return [float(x) for x in sys.stdin.readline().split()]
+def LS(): return sys.stdin.readline().split()
+def I(): return int(sys.stdin.readline())
+def F(): return float(sys.stdin.readline())
+def DP(N, M, first): return [[first] * M for n in range(N)]
+def DP3(N, M, L, first): return [[[first] * L for n in range(M)] for _ in range(N)]
+# }}}
+
+
+def solve():
+    a = input()
+    b = input()
+    print(135)
+
+if __name__ == "__main__":
+    solve()
+

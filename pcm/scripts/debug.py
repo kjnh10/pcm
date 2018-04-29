34fc9f49fbdff2c0081ef3586bf2ce2d05d27d2b
kjnh10 <kojinho10@gmail.com>
2018-04-29 21:01:10 +0900

    Add a script for debug

---
+++ pcm/scripts/debug.py
@@ -0,0 +1,6 @@
+from onlinejudge.implementation.main import main as oj
+from pcm.__main__ import prepare, submit, test
+
+# oj(['download', 'https://arc096.contest.atcoder.jp/tasks/arc096_a'])
+
+prepare(['https://arc096.contest.atcoder.jp/assignments'])

75ea3e8c6db65b2764090b91b6853f177bc92b06
kjnh10 <kojinho10@gmail.com>
2018-05-23 23:51:35 +0900

    Update C++ template

--- pcm/template/solve.cpp
+++ pcm/template/solve.cpp
@@ -1,9 +1,26 @@
-#include <iostream>
-
+#include <iostream>/*{{{*/
+#include <bits/stdc++.h>
+#define REP(i, x) for(int i = 0; i < (int)(x); i++)
+#define REPS(i,x) for(int i = 1; i <= (int)(x); i++)
+#define RREP(i,x) for(int i=((int)(x)-1);i>=0;i--)
+#define RREPS(i,x) for(int i=((int)(x));i>0;i--)
+#define FOR(i, m, n) for(int i = m;i < n;i++)
+#define RFOR(i, m, n) for(int i = m;i >= n;i--)
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) ((int)(x).size())
+#define FILL(x,y) memset(x,y,sizeof(x))
+#define SORT(v, n) sort(v, v+n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define PB(a) push_back(a)
+#define INF 999999999
 using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+int dy[]={0, 0, 1, -1, 0};
+int dx[]={1, -1, 0, 0, 0};/*}}}*/
 
 int main() {
-  string s;
-  cin >> s;
-  cout << s << endl;
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
 }

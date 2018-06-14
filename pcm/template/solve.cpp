a5cd375b25752f1aaf38d5548098358bb634d893
kjnh10 <kojinho10@gmail.com>
2018-06-14 23:33:10 +0900

    update cpp template

--- pcm/template/solve.cpp
+++ pcm/template/solve.cpp
@@ -1,9 +1,9 @@
 #include <iostream>/*{{{*/
 #include <bits/stdc++.h>
-#define REP(i, x) for(int i = 0; i < (int)(x); i++)
-#define REPS(i,x) for(int i = 1; i <= (int)(x); i++)
-#define RREP(i,x) for(int i=((int)(x)-1);i>=0;i--)
-#define RREPS(i,x) for(int i=((int)(x));i>0;i--)
+#define rep(i, x) for(int i = 0; i < (int)(x); i++)
+#define reps(i,x) for(int i = 1; i <= (int)(x); i++)
+#define rrep(i,x) for(int i=((int)(x)-1);i>=0;i--)
+#define rreps(i,x) for(int i=((int)(x));i>0;i--)
 #define FOR(i, m, n) for(int i = m;i < n;i++)
 #define RFOR(i, m, n) for(int i = m;i >= n;i--)
 #define ALL(x) (x).begin(),(x).end()
@@ -11,7 +11,7 @@
 #define FILL(x,y) memset(x,y,sizeof(x))
 #define SORT(v, n) sort(v, v+n);
 #define VSORT(v) sort(v.begin(), v.end());
-#define PB(a) push_back(a)
+#define pb(a) push_back(a)
 #define debug(x) cerr << __LINE__ << ": " << #x << " -> " << x << '\n'
 #define INF 999999999
 using namespace std;

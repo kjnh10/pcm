196ecae0018a0d365d5bb94195882ddcdb5656f5
kjnh10 <kojinho10@gmail.com>
2018-06-15 13:10:54 +0900

    Update cpp template

--- pcm/template/solve.cpp
+++ pcm/template/solve.cpp
@@ -6,17 +6,33 @@
 #define rreps(i,x) for(int i=((int)(x));i>0;i--)
 #define FOR(i, m, n) for(int i = m;i < n;i++)
 #define RFOR(i, m, n) for(int i = m;i >= n;i--)
-#define ALL(x) (x).begin(),(x).end()
-#define SZ(x) ((int)(x).size())
-#define FILL(x,y) memset(x,y,sizeof(x))
-#define SORT(v, n) sort(v, v+n);
-#define VSORT(v) sort(v.begin(), v.end());
+#define all(x) (x).begin(),(x).end()
+#define sz(x) ((int)(x).size())
+#define fill(x,y) memset(x,y,sizeof(x))
 #define pb(a) push_back(a)
-#define debug(x) cerr << __LINE__ << ": " << #x << " -> " << x << '\n'
+#define debug(x)                 cerr << __LINE__ << ": " << #x << " -> " << x << '\n'
+#define debug2(x, y)             cerr << __LINE__ << #x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define debug3(x, y, z)          cerr << __LINE__ << #x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define debug4(a, b, c, d)       cerr << __LINE__ << #a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define debug5(a, b, c, d, e)    cerr << __LINE__ << #a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define debug6(a, b, c, d, e, f) cerr << __LINE__ << #a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
 #define INF 999999999
 using namespace std;
 typedef long long ll;
-typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef long long ll;
+typedef vector<long long> vll;
+typedef vector<vll> vvll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef vector<pii> vii;
+typedef vector<vii> vvii;
+typedef tuple<int,int,int> iii;
+typedef set<int> si;
+typedef complex<double> pnt;
+typedef vector<pnt> vpnt;
+typedef priority_queue<pii,vii,greater<pii> > spq;
 int dy[]={0, 0, 1, -1, 0};
 int dx[]={1, -1, 0, 0, 0};/*}}}*/
 

e56564530d18e321cc3251850c0722632df9f651
kjnh10 <kojinho10@gmail.com>
2018-06-19 15:04:59 +0000

    update scripts

--- pcm/scripts/prepare_all.py
+++ pcm/scripts/prepare_all.py
@@ -1,9 +1,11 @@
 from pcm.__main__ import pp, sb, tt, ga
 import os
 import subprocess
+import shutil
 
 # work_dir = "/Users/koji0708/go/src/github.com/kjnh10/pcw/work/atcoder"
-work_dir = "/home/koji0708/go/src/github.com/kjnh10/pcw/work/atcoder"
+# work_dir = "/home/koji0708/go/src/github.com/kjnh10/pcw/work/atcoder"
+work_dir = "/home/vagrant/go/src/github.com/kjnh10/pcw/work/atcoder"
 
 def get_all(type, start, end):
     for num in range(start, end):
@@ -12,24 +14,31 @@ def get_all(type, start, end):
         print(f"start {snum}")
 
         if os.path.exists(work_dir + f"/{type}{snum}"):
-        #     print("skip")
-        #     continue
-        #
-        # subprocess.run(
-        #     ["pcm", "prepare", f"https://{type}{snum}.contest.atcoder.jp"],
-        #     stderr=subprocess.STDOUT,
-        #     check=True,
-        #     )
-        # # prepare([f"https://{type}{snum}.contest.atcoder.jp"])  # sys.exit()の使用の影響でループが止まる。
+            # print("skip")
+            # continue
 
-            os.chdir(work_dir + f"/{type}{snum}")
+
+            print("deleted")
+            shutil.rmtree(work_dir + f"/{type}{snum}")
+        
+        subprocess.run(
+            ["pcm", "pp", f"https://{type}{snum}.contest.atcoder.jp"],
+            stderr=subprocess.STDOUT,
+            check=True,
+            )
+        # prepare([f"https://{type}{snum}.contest.atcoder.jp"])  # sys.exit()の使用の影響でループが止まる。
+
+        os.chdir(work_dir + f"/{type}{snum}")
+        try:
             subprocess.run(
                 ["pcm", "ga"],
                 stderr=subprocess.STDOUT,
                 check=True,
                 )
+        except:
+            print("getting answers failed")
     print("finish")
 
 if __name__ == '__main__':
-    get_all('abc', 88, 100)
-    get_all('arc', 1, 99)
+    # get_all('abc', 88, 100)
+    get_all('arc', 58, 99)

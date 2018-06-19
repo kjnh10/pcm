52aafc5e555f5e27d03a2f853aeb4d0d90ba9c04
kjnh10 <kojinho10@gmail.com>
2018-06-19 17:19:07 +0000

    udpate template

--- pcm/template/solve.cpp
+++ pcm/template/solve.cpp
@@ -1,39 +1,30 @@
 using namespace std;
-#include <iostream>/*{{{*/
-struct Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
-
+#include <iostream>
 #include <bits/stdc++.h>
 
-#define rep(i, x) for(int i = 0; i < (int)(x); i++)
-#define reps(i,x) for(int i = 1; i <= (int)(x); i++)
-#define rrep(i,x) for(int i=((int)(x)-1);i>=0;i--)
-#define rreps(i,x) for(int i=((int)(x));i>0;i--)
-#define FOR(i, m, n) for(int i = m;i < n;i++)
-#define RFOR(i, m, n) for(int i = m;i >= n;i--)
-#define foreach(x,a) for(auto& (x) : (a) )
-#define all(x) (x).begin(),(x).end()
-#define sum(v) accumulate(all(v), 0)
-#define sz(x) ((int)(x).size())
-#define fill(x,y) memset(x,y,sizeof(x))
-#define pb(a) push_back(a)
 // dump macro{{{
 // http://www.creativ.xyz/dump-cpp-652
 #define DUMPOUT cerr // 変数ダンプ先。coutかcerr
 
-#define DEBUG_ // 提出時はコメントアウト
-#ifdef DEBUG_
-#define DEB
-#else
-// DEB と打つとデバッグ時以外はコメントアウトになる
-#define DEB /##/
-#endif
-
-// 変数ダンプ用マクロ。デバッグ時以外は消滅する
+#ifdef PCM
 // 引数はいくつでもどんな型でも可（ストリーム出力演算子があればOK）
-#define dump(...) DEB DUMPOUT<<"  "; \
-DUMPOUT<<#__VA_ARGS__; \
-DUMPOUT<<":=> "; \
-dump_func(__VA_ARGS__); DUMPOUT<<"in ["<<__LINE__<<":"<<__FUNCTION__<<"]"<<endl;
+#define dump(...) DUMPOUT<<"  "; \
+  DUMPOUT<<#__VA_ARGS__; \
+  DUMPOUT<<":=> "; \
+  dump_func(__VA_ARGS__); DUMPOUT<<"in ["<<__LINE__<<":"<<__FUNCTION__<<"]"<<endl;
+#define dump_1d(x,n) DUMPOUT <<"  " \
+  <<#x<<"["<<#n<<"]"<<":=> {"; \
+  rep(i,n){ DUMPOUT << x[i] << (((i)==(n-1))?"}":", "); } DUMPOUT <<" in [" << __LINE__ << "]" << endl;
+
+#define dump_2d(x,n,m) DUMPOUT <<"  " \
+    <<#x<<"["<<#n<<"]"<<"["<<#m<<"]"<<":=> \n"; \
+    rep(i,n)rep(j,m){ DUMPOUT << ((j==0)?"     |":" ") << x[i][j] << (((j)==(m-1))?"|\n":" "); } \
+    DUMPOUT <<"  in [" << __LINE__ << "]" << endl;
+#else
+#define dump(...) 42
+#define dump_1d(...) 42
+#define dump_2d(...) 42
+#endif
 
 // デバッグ用変数ダンプ関数
 void dump_func() {
@@ -61,21 +52,27 @@ ostream& operator << (ostream& os, vector<T>& vec) {
     os << "}";
     return os;
 }/*}}}*/
-#define debug(x)                 cerr << __LINE__ << ": " << #x << " -> " << x << '\n'/*{{{*/
-#define debug2(x, y)             cerr << __LINE__ << #x<<": "<<x<<" | "<<#y<<": "<<y<<endl
-#define debug3(x, y, z)          cerr << __LINE__ << #x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
-#define debug4(a, b, c, d)       cerr << __LINE__ << #a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
-#define debug5(a, b, c, d, e)    cerr << __LINE__ << #a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
-#define debug6(a, b, c, d, e, f) cerr << __LINE__ << #a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
-#define db_2d(d,n,m) rep(i,n){rep(j,m){ cerr << (d)[i][j] << (((j)==(m-1))?"\n":" "); }}
-#define db_1d(d,n) rep(i,n){cout << d[i] << (((i)==(n-1))?"\n":" ") << __LINE__}/*}}}*/
+
+#define rep(i, x) for(int i = 0; i < (int)(x); i++)/*{{{*/
+#define reps(i,x) for(int i = 1; i <= (int)(x); i++)
+#define rrep(i,x) for(int i=((int)(x)-1);i>=0;i--)
+#define rreps(i,x) for(int i=((int)(x));i>0;i--)
+#define FOR(i, m, n) for(int i = m;i < n;i++)
+#define RFOR(i, m, n) for(int i = m;i >= n;i--)
+#define foreach(x,a) for(auto& (x) : (a) )
+#define all(x) (x).begin(),(x).end()
+#define sum(v) accumulate(all(v), 0)
+#define sz(x) ((int)(x).size())
+#define fill(x,y) memset(x,y,sizeof(x))
+#define pb(a) push_back(a)
 #define INF 2147483647
 #define INFLL 1000000000000000000LL
-#define MOD 1000000007
+#define MOD 1000000007/*}}}*/
+
+struct Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;/*{{{*/
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
-typedef long long ll;
 typedef vector<long long> vll;
 typedef vector<vll> vvll;
 typedef long double ld;
@@ -88,7 +85,11 @@ typedef complex<double> pnt;
 typedef vector<pnt> vpnt;
 typedef priority_queue<pii,vii,greater<pii> > spq;
 int dy[]={0, 0, 1, -1, 0};
-int dx[]={1, -1, 0, 0, 0};/*}}}*/
+int dx[]={1, -1, 0, 0, 0};
+
+/*}}}*/
+
+// global variables
 
 int main() {
 

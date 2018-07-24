493451cdf546bf5cad8ffe07fe2cf7a4ddb2c57a
kjnh10 <kojinho10@gmail.com>
2018-07-24 12:28:13 +0900

    update template

--- pcm/template/codes/solve.cpp
+++ pcm/template/codes/solve.cpp
@@ -11,7 +11,7 @@ using namespace std;
 // #define rep(i, x) for(int i = 0; i < (int)(x); i++)
 #define _overload3(_1,_2,_3,name,...) name
 #define _rep(i,n) repi(i,0,n)
-#define repi(i,a,b) for(int i=int(a);i<int(b);++i)
+#define repi(i,a,b) for(int i=(int)(a);i<(int)(b);++i)
 #define rep(...) _overload3(__VA_ARGS__,repi,_rep,)(__VA_ARGS__)
 #define reps(i,x) for(int i = 1; i <= (int)(x); i++)
 #define rrep(i,x) for(int i=((int)(x)-1);i>=0;i--)

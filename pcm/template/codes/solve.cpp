7c2b64e4d76013a80dcce7ad1aa3456235af56a8
kjnh10 <kojinho10@gmail.com>
2018-08-05 13:35:29 +0900

    Add test function to cpp template

--- pcm/template/codes/solve.cpp
+++ pcm/template/codes/solve.cpp
@@ -75,10 +75,18 @@ int solve(){
   return 0;
 }
 
+int test(){ //{{{
+  return 0;
+}
+//}}}
+
 signed main() { //{{{
 #ifdef INPUT_FROM_FILE
   std::ifstream in(infile);
   std::cin.rdbuf(in.rdbuf());
+#endif
+#ifdef PCM
+  test();
 #endif
   solve();
   return 0;

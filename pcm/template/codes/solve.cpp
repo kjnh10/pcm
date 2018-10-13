a9331ec092cf7c9a0092a07fc017943cc7a9241b
kjnh10 <kojinho10@gmail.com>
2018-10-14 04:40:13 +0900

    update template

--- pcm/template/codes/solve.cpp
+++ pcm/template/codes/solve.cpp
@@ -1,15 +1,8 @@
-// template version 1.2
+// template version 1.3
+// {{{ define basic macro
 using namespace std;
 #include <iostream>
 #include <bits/stdc++.h>
-
-#define infile "../test/sample-1.in"
-#define int long long
-// #define INF 2147483647
-#define INF 1000000000000000000LL
-#define MOD 1000000007LL
-
-// {{{ define basic macro
 #define _overload3(_1,_2,_3,name,...) name
 #define _rep(i,n) repi(i,0,n)
 #define repi(i,a,b) for(int i=(int)(a);i<(int)(b);++i)
@@ -17,10 +10,6 @@ using namespace std;
 #define _rrep(i,n) rrepi(i,0,n)
 #define rrepi(i,a,b) for(int i=(int)(b-1);i>=(int)(a);--i)
 #define rrep(...) _overload3(__VA_ARGS__,rrepi,_rrep,)(__VA_ARGS__)
-
-#define foreach(x,a) for(auto& (x) : (a) )
-#define each(it,c) for(__typeof((c).begin()) it=(c).begin();it!=(c).end();it++)
-
 #define all(x) (x).begin(),(x).end()
 #define sum(v) accumulate(all(v), 0LL)
 #define sz(x) ((int)(x).size())
@@ -28,17 +17,10 @@ using namespace std;
 #define mp(a, b) make_pair(a, b)
 #define uni(x) sort(all(x));x.erase(unique(all(x)),x.end())
 #define ten(n) ((int)1e##n)
-
+template<class T, size_t N> size_t sza(const T (&array)[N]) { return N; }
 template<class T> inline void chmax(T &a, const T &b) { if(a < b) a = b; }
 template<class T> inline void chmin(T &a, const T &b) { if(a > b) a = b; }
-
-template<typename A, size_t N, typename T> // n次元配列の初期化。第２引数の型のサイズごとに初期化していく。
-void Fill(A (&array)[N], const T &val){
-    std::fill( (T*)array, (T*)(array+N), val );
-}
-
-template <class T = int>
-T in() {T x; cin>>x; return (x);}
+template<class T=int> T in() {T x; cin>>x; return (x);}
 
 struct Fast {
   Fast(){
@@ -46,8 +28,8 @@ struct Fast {
     ios::sync_with_stdio(false);
   }
 } fast;
-// }}}
-//{{{ dump macro
+
+// dump macro
 #ifdef PCM
   #include "dump.hpp"
 #else
@@ -55,39 +37,40 @@ struct Fast {
   #define dump_1d(...) 42
   #define dump_2d(...) 42
 #endif
-//}}}
-//{{{ others
+
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef long double ld;
 typedef pair<int,int> pii;
 typedef tuple<int,int,int> iii;
-template<typename T>
-using PQ = priority_queue<T, vector<T>, greater<T>>;
-
+template<typename T> using PQ = priority_queue<T, vector<T>, greater<T>>;
 int dx[]={1, -1, 0, 0};
 int dy[]={0, 0, 1, -1};
-//}}}
+#define fi first
+#define se second
 
+// }}}
+// varible setting{{{
+
+#define infile "../test/sample-1.in"
+#define int long long
+#define INF 1000000000000000000LL
+// #define INF 2147483647
+#define MOD 1000000007LL
+
+// }}}
 
 int solve(){
-
+  int n=3;
+  dump(3);
   return 0;
 }
-
-int test(){ //{{{
-  return 0;
-}
-//}}}
 
 signed main() { //{{{
 #ifdef INPUT_FROM_FILE
   std::ifstream in(infile);
   std::cin.rdbuf(in.rdbuf());
-#endif
-#ifdef PCM
-  test();
 #endif
   solve();
   return 0;

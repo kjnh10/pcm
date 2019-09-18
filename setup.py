b48c772e22107c60ed777e41b0952674de9ed036
kjnh10 <kojinho10@gmail.com>
2019-09-18 17:39:11 +0900

    release v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='pcm',
-    version='0.1',
-    install_requires=['Click', 'online-judge-tools', 'toml'],
-    entry_points={'console_scripts': ['pcm=pcm.__main__:cli']}
+    version='0.2',
+    description="tiny programing contest manager",
+    author='kjnh10',
+    author_email='kojinho10@gmail.com',
+    url='https://github.com/kjnh10/pcm',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['Click>=7.0', 'online-judge-tools', 'toml'],
+    entry_points={'console_scripts': ['pcm=pcm.__main__:cli']},
+    python_requires='>=3.5',
 )

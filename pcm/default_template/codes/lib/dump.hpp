00f0faf530b57ccbcee5f5629bd28ff2dda7bb4a
kjnh10 <kojinho10@gmail.com>
2020-02-19 01:40:41 +0900

    dumpをnestして使えるように変更

--- pcm/default_template/codes/lib/dump.hpp
+++ pcm/default_template/codes/lib/dump.hpp
@@ -5,15 +5,15 @@ using namespace std;
 #define DUMPOUT cerr // where to dump. cout or cerr
 
 namespace dump_macro{
-  vector<string> varnames;
-  int varidx;
+  stack<vector<string>> varnames;
+  stack<int> varidx;
 }
 
 #define cerrendl cerr << endl
 
 #define dump(...)  \
   {  \
-    dump_macro::varnames = [](string s) -> vector<string> { \
+    dump_macro::varnames.push([](string s) -> vector<string> { \
       int n = s.size(); \
       vector<string> res; \
       string tmp = ""; \
@@ -32,9 +32,10 @@ namespace dump_macro{
       } \
       res.push_back(tmp); \
       return res; \
-    }(#__VA_ARGS__); \
-    dump_macro::varidx = 0; \
+    }(#__VA_ARGS__)); \
+    dump_macro::varidx.push(0); \
     dump_func(__VA_ARGS__); DUMPOUT<<"in ["<<__LINE__<<":"<<__FUNCTION__<<"]"<<endl;  \
+    dump_macro::varnames.pop();dump_macro::varidx.pop(); \
   }
 
 #define dump_1d(x,n)  \
@@ -57,14 +58,14 @@ void dump_func() {
 template <class Head, class... Tail>
 void dump_func(Head&& head, Tail&&... tail)
 {
-    DUMPOUT << dump_macro::varnames[dump_macro::varidx] << ":" << head;
+    DUMPOUT << dump_macro::varnames.top()[dump_macro::varidx.top()] << ":" << head;
     if (sizeof...(Tail) == 0) {
         DUMPOUT << " ";
     }
     else {
         DUMPOUT << ", ";
     }
-    ++dump_macro::varidx;
+    ++dump_macro::varidx.top();
     dump_func(std::move(tail)...);
 }
 

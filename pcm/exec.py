e2b9ff20808379e25556c71a41ed40e32b22f7cd
kjnh10 <kojinho10@gmail.com>
2020-05-27 20:36:09 +0900

    windowsでメモリ使用量を取得できるように修正

--- pcm/exec.py
+++ pcm/exec.py
@@ -1,22 +1,32 @@
 import subprocess
 import sys
-import resource
+try:
+    import resource
+except:
+    pass
+import psutil
 
 # resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrssは最初に実行したプロセスの情報しかとれないようなので
 # compileの子プロセスなども本体の方では走る可能性があるのでこちらで単独で実行する事でsolution codeのstaticを取得する。
+# psutil for windows
 
 command = sys.argv[1]
 command = ['python', command] if '.py' in command else command
 
+pre_used_memory = psutil.Process().memory_info().peak_wset / (1<<20)
 subprocess.run(
         command,
         stdin = open(sys.argv[2], 'r') if sys.argv[2]!='None' else None,
         stdout = subprocess.DEVNULL,
         stderr = subprocess.DEVNULL,
         )
+after_used_memory = psutil.Process().memory_info().peak_wset
 
-rs = resource.getrusage(resource.RUSAGE_CHILDREN)
-used_memory = rs.ru_maxrss / (1<<10) # MB unit
+try:
+    rs = resource.getrusage(resource.RUSAGE_CHILDREN)
+    used_memory = rs.ru_maxrss / (1<<10) # MB unit
+except:
+    used_memory = (after_used_memory - pre_used_memory) / (1<<20)
 print(used_memory)
 # print(rs.ru_utime)  # 大体本体の時間と一致するがとりあえずこちらは使用しない。
 # print(rs.ru_stime)

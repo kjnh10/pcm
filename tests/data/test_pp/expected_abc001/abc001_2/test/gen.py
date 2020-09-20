ce219f4a567dd0963cc174ecba9e1a50d948792c
kjnh10 <kojinho10@gmail.com>
2020-09-20 17:33:04 +0900

    fix test data

--- tests/data/test_pp/expected_abc001/abc001_2/test/gen.py
+++ tests/data/test_pp/expected_abc001/abc001_2/test/gen.py
@@ -1,11 +1,8 @@
-from random import randint, choice, sample, choices
+from random import choice, sample, choices
+from random import randint as rint
 from typing import TYPE_CHECKING, List, Optional, Type
 import string
 import math
-# print(choice(a))  # 1つ選択
-# print(sample(a, k=2))  # 非復元抽出
-# print(sample(a, k=len(L)))  # random permutation
-# print(choices(a, k=2))  # 復元抽出
 
 class UnionFind():
     def __init__(self, n):
@@ -54,13 +51,13 @@ class UnionFind():
     def __str__(self):
         return '\n'.join('{}: {}'.format(r, self.members(r)) for r in self.roots())
 
-class randperm(object):
+class rperm(object):
     def __init__(self, n:int):
         self.data = sample(list(range(1, n+1)), k=n)
     def __str__(self):
         return ' '.join(map(str, self.data))
 
-class randseq(object):
+class rseq(object):
     def __init__(self, n: int, l: int, r: int, distinct=False):
         self.data = []
         used = set()
@@ -68,7 +65,7 @@ class randseq(object):
             raise Exception(print("n>r-l+1 and distinct=True is not impossible"))
 
         while len(self.data) < n:
-            v = randint(l, r)
+            v = rint(l, r)
             if distinct and v in used:
                 pass
             else:
@@ -78,14 +75,14 @@ class randseq(object):
     def __str__(self):
         return ' '.join(map(str, self.data))
 
-def randstr(length: int, chars: List=['a', 'b', 'c', 'd', 'e']):
-# def randstr(length: int, chars: List=string.ascii_lowercase):
+def rstr(length: int, chars: List=['a', 'b', 'c', 'd', 'e']):
+# def rstr(length: int, chars: List=string.ascii_lowercase):
     res = ""
     for i in range(length):
         res += choice(chars)
     return res
 
-def randprime(l: int = 2, r: int = 1000000007): # [l, r]
+def rprime(l: int = 2, r: int = 1000000007): # [l, r]
     def is_prime(x):
         if (x == 1): return False
         for i in range(2, int(math.sqrt(x))+1):
@@ -96,14 +93,14 @@ def randprime(l: int = 2, r: int = 1000000007): # [l, r]
 
     cnt = 0
     while True:
-        res = randint(l, r)
+        res = rint(l, r)
         if (is_prime(res)):
             return res
         cnt += 1
         if cnt>=1000:
             assert False
 
-class randtree(object):
+class rtree(object):
     def __init__(self, n: int, root: int = 0):
         assert root < n
         self.edges = []
@@ -127,7 +124,7 @@ class randtree(object):
             res.append(f"{edge[0]+one_index} {edge[1]+one_index}")
         return '\n'.join(res)
 
-class randgraph(object):  # undirected
+class rgraph(object):  # undirected
     def __init__(self, n:int, lb:int=1, ub:int=float('inf'), tree_ok=True):
         self.n = n
         if ub == float('inf'):
@@ -136,9 +133,9 @@ class randgraph(object):  # undirected
         while True:
             if tree_ok:  # tree graph
                 # 10%くらいは木が生成されるように
-                r = randint(1, 10)
+                r = rint(1, 10)
                 if (r<=2):
-                    self.edges = randtree(n).edges
+                    self.edges = rtree(n).edges
                     return
 
             # TODO: [line graph, star graph, complete graph]
@@ -146,8 +143,8 @@ class randgraph(object):  # undirected
             uf = UnionFind(n)
             self.edges = set()
             while(uf.group_count()>1 or len(self.edges)<lb):
-                u = randint(0, n-1)
-                v = randint(0, n-1)
+                u = rint(0, n-1)
+                v = rint(0, n-1)
                 if (u==v): continue
 
                 if (u>v): u,v=v,u
@@ -167,5 +164,19 @@ class randgraph(object):  # undirected
 
 # write down here
 # ---------------------------------------------
-n = randint(2, 10)
-print(n)
+def generator():  # you should implement as generator
+    # for some specified ranges
+    # for a in range(1, 5):
+    #     for b in range(1, 5):
+    #         # single case
+    #         print(a, b)
+    #         yield 0
+
+    while True:
+        generate_random_case()
+        yield
+
+def generate_random_case(): # single case
+    a = rint(2, 10)
+    b = rint(2, 10)
+    print(a, b)
